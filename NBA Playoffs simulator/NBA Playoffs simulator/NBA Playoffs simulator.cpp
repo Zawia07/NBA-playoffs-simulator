eb8663f5721ccb6dea2f133e3c88f141dfbf997b
Mohammed Zawia <mzawia07@gmail.com>
2025-05-19 17:54:06 +0800

    Western Conference is finally done
    Onto EC which shouldn't take long at all.
    Plata O Plomo Iykykykyk 😂

--- NBA Playoffs simulator/NBA Playoffs simulator/NBA Playoffs simulator.cpp
+++ NBA Playoffs simulator/NBA Playoffs simulator/NBA Playoffs simulator.cpp
@@ -4,13 +4,7 @@
 #include <iostream>
 using namespace std;
 
-void randomTeams()
-{
-
-}
-void WcUserInput()
-{
-    string wcTeams[15] = {
+string wcTeams[15] = {
         "Denver Nuggets", "Phoenix Suns", "Golden State Warriors", 
         "Los Angeles Lakers", "Los Angeles Clippers", "Sacramento Kings", 
         "Minnesota Timberwolves", "Dallas Mavericks", "New Orleans Pelicans", 
@@ -18,10 +12,30 @@ void WcUserInput()
         "San Antonio Spurs", "Utah Jazz", "Portland Trail Blazers"
     };
 
-    string wcPlayoffTeams[8];
-    string wcR2PlayoffTeams[4];
-    string wcWcfPlayoffTeams[4];
-    int numberOfTeams = 0;
+string wcPlayoffTeams[8];
+string wcR2PlayoffTeams[4];
+string WcfPlayoffTeams[2];
+
+string ecTeams[15] = {
+        "Boston Celtics", "Milwaukee Bucks", "Philadelphia 76ers", 
+        "Cleveland Cavaliers", "New York Knicks", "Miami Heat", 
+        "Atlanta Hawks", "Toronto Raptors", "Chicago Bulls", 
+        "Indiana Pacers", "Brooklyn Nets", "Charlotte Hornets", 
+        "Detroit Pistons", "Orlando Magic", "Washington Wizards"
+    };
+
+string ecPlayoffTeams[8];
+string ecR2PlayoffTeams[4];
+string ecWcfPlayoffTeams[2];
+
+string playoffFinalsTeams[2];
+
+void randomTeams()
+{
+
+}
+void WcUserInput()
+{
     // Print teams 0 to 14
     for (int i = 0; i < 15; i++) {
         
@@ -49,32 +63,63 @@ void WcUserInput()
         cout <<"No." << i+1 << " " << wcPlayoffTeams[i] << endl;
     }
 
-    cout << "\nChoose 4 teams (by number 1-15) to advance to the Western Conference playoffs:\n";
+    cout << "\nChoose 4 teams (by number 1-8) to advance to the Western Conference playoffs' second round:\n" << endl;
 
     for (int i = 0; i < 4; i++)
     {
         int choice;
-        cout << "Team No" << i+1 << " :";
+        cout << "Team No." << i+1 << ": ";
         cin >> choice;
 
-        while (choice < 0 || choice >= 8) {
+        while (choice < 1 || choice >= 9) {
             cout << "Invalid input. Try again (1-8): ";
             cin >> choice;
         }
 
-        wcR2PlayoffTeams[i]=wcPlayoffTeams[choice];
+        wcR2PlayoffTeams[i]=wcPlayoffTeams[choice-1];
     }
 
-    cout << "The teams progressing to the Western Conference 2nd round are: \n";
+    cout << "The teams progressing to the Western Conference 2nd round are: \n" << endl;
 
     for (int i = 0; i < 4; i++)
     {
         /* code */
         cout << "Team No." << i+1 << " " << wcR2PlayoffTeams[i] << endl;
     }
+
+    cout << "\nChoose 2 teams (by number 1-4) to advance to the Western Conference Finals\n" << endl;
     
+    for (int i = 0; i < 2; i++)
+    {
+        int choice;
+        cout << "Team No." << i+1 << ": ";
+        cin >> choice;
+
+        while (choice < 0 || choice >= 5) {
+            cout << "Invalid input. Try again (1-4): ";
+            cin >> choice;
+        }
+
+        WcfPlayoffTeams[i]=wcR2PlayoffTeams[choice-1];
+    }
     
+    cout << "\nThe teams playing for a finals spot are \n";
+
+    for (int i = 0; i < 2; i++)
+    {
+        /* code */
+        cout << "Team No." << i+1 << " " << WcfPlayoffTeams[i] << endl;
+    }
+
+    int answer;
+    cout << "The team progressing to the finals is ";
+    cin >> answer;
     
+    playoffFinalsTeams[0] = WcfPlayoffTeams[answer -1];
+
+    cout << endl << "The team progressing to the NBA finals is: " << playoffFinalsTeams[0];
+    
+
 }
 void r1EcUserInput()
 {
